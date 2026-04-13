bd91fc87ec6e9e401e0422f447eec78eb02a61c9
Abdelouahed Aliali <aaliali@e1r3p4.1337.ma>
2026-04-13 07:57:30 +0100

    reer

--- products/views.py
+++ products/views.py
@@ -4048,7 +4048,7 @@ def getconnectedusers(request):
     trs=''
     for i in connected:
 
-        print("jdfjwfjsdjfwlfjkjfkjwr", i.user.groups)
+        print("jdfjwfjsdjfwlfjkjfkjwr", i.user.groups.all())
         if i.user.groups.filter(name='clients').exists():
             trs+=f"""
             <tr>
